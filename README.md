6cbb184498fe0919c11687e34e946aa3aafaeaef
annadan1 <anna_s92_@mail.ru>
2023-03-01 08:39:59 +0700

    Add start page.

---
+++ README.md
@@ -0,0 +1,5 @@
+# conquest
+
+You can view the page by downloading the repository. 
+Then enter the command - make start. 
+The page will open on port 3000.
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/style.css';
+import './css/header.css';
+import './css/main.css';
+import './css/populars.css';
+import './css/banner.css';
+import './css/socialmedia.css';
+import './css/footer.css';
